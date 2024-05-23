c6579856466ff890321e4d7d9657d5315f67fd0e
Guilherme-14 <132360130+Guilherme-14@users.noreply.github.com>
2024-05-23 12:03:03 -0300

    Som_Desafio2

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_desafio2/principal.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
